55afcdec70da617b5f263359fc788aae46249adf
Aaron William Po <apo2@uwo.ca>
2023-05-09 08:36:05 -0400

    Begin work on account page

--- src/pages/account/index.tsx
+++ src/pages/account/index.tsx
@@ -1,13 +1,64 @@
+import UserContext from '@/contexts/userContext';
+import withPageAuthRequired from '@/util/withPageAuthRequired';
 import { NextPage } from 'next';
+
+import { useContext } from 'react';
+import { Tab } from '@headlessui/react';
+import Head from 'next/head';
 
 interface AccountPageProps {}
 
 const AccountPage: NextPage<AccountPageProps> = () => {
+  const { user } = useContext(UserContext);
+
   return (
-    <div>
-      <h1>Account Page</h1>
-    </div>
+    <>
+      <Head>
+        <title>Your Account | The Biergarten App</title>
+        <meta
+          name="description"
+          content="Your account page. Here you can view your account information, change your settings, and view your posts."
+        />
+      </Head>
+      <div className="flex h-full flex-col items-center bg-base-300">
+        <div className="m-12 flex w-9/12 flex-col items-center justify-center space-y-3">
+          <div className="flex flex-col items-center space-y-3">
+            <div className="avatar">
+              <div className="bg-base-black w-24 rounded-full bg-slate-700" />
+            </div>
+
+            <div className="flex flex-col items-center space-y-1">
+              <p className="text-3xl font-bold">Hello, {user?.username}!</p>
+              <p className="text-lg">Welcome to your account page.</p>
+            </div>
+          </div>
+
+          <div className="w-full">
+            <Tab.Group>
+              <Tab.List className="tabs tabs-boxed items-center justify-center rounded-2xl">
+                <Tab className="tab tab-md w-1/3 uppercase ui-selected:tab-active">
+                  Settings
+                </Tab>
+                <Tab className="tab tab-md w-1/3 uppercase ui-selected:tab-active">
+                  Account Info
+                </Tab>
+                <Tab className="tab tab-md w-1/3 uppercase ui-selected:tab-active">
+                  Your Posts
+                </Tab>
+              </Tab.List>
+              <Tab.Panels>
+                <Tab.Panel>Content 1</Tab.Panel>
+                <Tab.Panel>Content 2</Tab.Panel>
+                <Tab.Panel>Content 3</Tab.Panel>
+              </Tab.Panels>
+            </Tab.Group>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
 export default AccountPage;
+
+export const getServerSideProps = withPageAuthRequired();
