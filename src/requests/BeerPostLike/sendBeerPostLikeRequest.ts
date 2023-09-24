39980eb8c326d74d25103d41d22e95c7cad163c4
Aaron William Po <apo2@uwo.ca>
2023-09-24 01:40:18 -0400

    chore: begin work on documenting client side requests code

--- src/requests/BeerPostLike/sendBeerPostLikeRequest.ts
+++ src/requests/BeerPostLike/sendBeerPostLikeRequest.ts
@@ -1,5 +1,12 @@
 import APIResponseValidationSchema from '@/validation/APIResponseValidationSchema';
 
+/**
+ * Sends a POST request to the server to like or unlike a beer post.
+ *
+ * @param beerPostId The ID of the beer post to like or unlike.
+ * @returns An object containing a success boolean and a message string.
+ * @throws An error if the response is not ok or if the API response is invalid.
+ */
 const sendBeerPostLikeRequest = async (beerPostId: string) => {
   const response = await fetch(`/api/beers/${beerPostId}/like`, {
     method: 'POST',
@@ -10,7 +17,6 @@ const sendBeerPostLikeRequest = async (beerPostId: string) => {
   }
 
   const data = await response.json();
-
   const parsed = APIResponseValidationSchema.safeParse(data);
 
   if (!parsed.success) {
