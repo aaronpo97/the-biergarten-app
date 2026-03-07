ef27d6f5531ebd72f92c510f9f4da04deb1a0891
Aaron Po <apo2@uwo.ca>
2026-03-06 23:13:01 -0500

    Create ConfirmationService class file and extract out of interface file

--- src/Core/Service/Service.Auth/IConfirmationService.cs
+++ src/Core/Service/Service.Auth/IConfirmationService.cs
@@ -9,27 +9,3 @@ public interface IConfirmationService
 {
     Task<ConfirmationServiceReturn> ConfirmUserAsync(string confirmationToken);
 }
-
-public class ConfirmationService(IAuthRepository authRepository, ITokenService tokenService)
-    : IConfirmationService
-{
-    public async Task<ConfirmationServiceReturn> ConfirmUserAsync(
-        string confirmationToken
-    )
-    {
-        var validatedToken = await tokenService.ValidateConfirmationTokenAsync(
-            confirmationToken
-        );
-
-        var user = await authRepository.ConfirmUserAccountAsync(
-            validatedToken.UserId
-        );
-
-        if (user == null)
-        {
-            throw new UnauthorizedException("User account not found");
-        }
-
-        return new ConfirmationServiceReturn(DateTime.UtcNow, user.UserAccountId);
-    }
-}
