2db234c0c305af97ae9f09e3985cf8f539ed5923
Aaron Po <apo2@uwo.ca>
2026-07-19 04:10:32 -0400

    Formatted includes/web_client

--- tooling/pipeline/includes/web_client/web_client.h
+++ tooling/pipeline/includes/web_client/web_client.h
@@ -12,24 +12,24 @@
  * @brief Abstract web client interface.
  */
 class WebClient {
- public:
-  virtual ~WebClient() = default;
+  public:
+   virtual ~WebClient() = default;
 
-  /**
-   * @brief Executes an HTTP GET request.
-   *
-   * @param url Request URL.
-   * @return Response body.
-   */
-  virtual std::string Get(const std::string& url) = 0;
+   /**
+    * @brief Executes an HTTP GET request.
+    *
+    * @param url Request URL.
+    * @return Response body.
+    */
+   virtual std::string Get(const std::string& url) = 0;
 
-  /**
-   * @brief URL-encodes a string value.
-   *
-   * @param value Raw string value.
-   * @return Encoded value safe for URL usage.
-   */
-  virtual std::string EncodeURL(const std::string& value) = 0;
+   /**
+    * @brief URL-encodes a string value.
+    *
+    * @param value Raw string value.
+    * @return Encoded value safe for URL usage.
+    */
+   virtual std::string EncodeURL(const std::string& value) = 0;
 };
 
 #endif  // BIERGARTEN_PIPELINE_INCLUDES_WEB_CLIENT_WEB_CLIENT_H_
