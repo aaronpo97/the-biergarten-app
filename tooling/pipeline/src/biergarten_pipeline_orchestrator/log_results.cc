7550418ad0959c0f7ecbcf42e9ed05f3ce7cda2a
Aaron Po <apo2@uwo.ca>
2026-07-19 04:10:33 -0400

    Formatted src/biergarten_pipeline_orchestrator

--- tooling/pipeline/src/biergarten_pipeline_orchestrator/log_results.cc
+++ tooling/pipeline/src/biergarten_pipeline_orchestrator/log_results.cc
@@ -12,48 +12,48 @@
 #include "services/logging/logger.h"
 
 void BiergartenPipelineOrchestrator::LogResults() const {
-  boost::json::array brewery_output;
+   boost::json::array brewery_output;
 
-  for (const auto& [address, brewery] : generated_breweries_) {
-    const City& city = address.city;
-    brewery_output.push_back(boost::json::object{
-        {"name_en", brewery.name_en},
-        {"description_en", brewery.description_en},
-        {"name_local", brewery.name_local},
-        {"description_local", brewery.description_local},
-        {"address", boost::json::object{
-                        {"city", city.city},
-                        {"country", city.country},
-                        {"state_province", city.state_province},
-                        {"iso3166_2", city.iso3166_2},
-                        {"postal_code", address.postal_code},
-                    }}});
-  }
+   for (const auto& [address, brewery] : generated_breweries_) {
+      const City& city = address.city;
+      brewery_output.push_back(boost::json::object{
+          {"name_en", brewery.name_en},
+          {"description_en", brewery.description_en},
+          {"name_local", brewery.name_local},
+          {"description_local", brewery.description_local},
+          {"address", boost::json::object{
+                          {"city", city.city},
+                          {"country", city.country},
+                          {"state_province", city.state_province},
+                          {"iso3166_2", city.iso3166_2},
+                          {"postal_code", address.postal_code},
+                      }}});
+   }
 
-  std::ostringstream brewery_oss;
-  PrettyPrint(brewery_oss, brewery_output);
-  logger_->Log({.level = LogLevel::Info,
-                .phase = PipelinePhase::Teardown,
-                .message = brewery_oss.str()});
+   std::ostringstream brewery_oss;
+   PrettyPrint(brewery_oss, brewery_output);
+   logger_->Log({.level = LogLevel::Info,
+                 .phase = PipelinePhase::Teardown,
+                 .message = brewery_oss.str()});
 
-  boost::json::array user_output;
+   boost::json::array user_output;
 
-  for (const auto& generated_user : generated_users_) {
-    user_output.push_back(boost::json::object{
-        {"first_name", generated_user.user.first_name},
-        {"last_name", generated_user.user.last_name},
-        {"gender", generated_user.user.gender},
-        {"username", generated_user.user.username},
-        {"bio", generated_user.user.bio},
-        {"activity_weight", generated_user.user.activity_weight},
-        {"email", generated_user.email},
-        {"date_of_birth", generated_user.date_of_birth},
-    });
-  }
+   for (const auto& generated_user : generated_users_) {
+      user_output.push_back(boost::json::object{
+          {"first_name", generated_user.user.first_name},
+          {"last_name", generated_user.user.last_name},
+          {"gender", generated_user.user.gender},
+          {"username", generated_user.user.username},
+          {"bio", generated_user.user.bio},
+          {"activity_weight", generated_user.user.activity_weight},
+          {"email", generated_user.email},
+          {"date_of_birth", generated_user.date_of_birth},
+      });
+   }
 
-  std::ostringstream user_oss;
-  PrettyPrint(user_oss, user_output);
-  logger_->Log({.level = LogLevel::Info,
-                .phase = PipelinePhase::Teardown,
-                .message = user_oss.str()});
+   std::ostringstream user_oss;
+   PrettyPrint(user_oss, user_output);
+   logger_->Log({.level = LogLevel::Info,
+                 .phase = PipelinePhase::Teardown,
+                 .message = user_oss.str()});
 }
--- tooling/pipeline/src/biergarten_pipeline_orchestrator/query_cities_with_countries.cc
+++ tooling/pipeline/src/biergarten_pipeline_orchestrator/query_cities_with_countries.cc
@@ -14,37 +14,36 @@
 #include "services/curated_data/curated_json_data_service.h"
 #include "services/logging/logger.h"
 
-std::vector<City>
-BiergartenPipelineOrchestrator::QueryLocations() {
-  logger_->Log({.level = LogLevel::Info,
-                .phase = PipelinePhase::Startup,
-                .message = "=== GEOGRAPHIC DATA OVERVIEW ==="});
+std::vector<City> BiergartenPipelineOrchestrator::QueryLocations() {
+   logger_->Log({.level = LogLevel::Info,
+                 .phase = PipelinePhase::Startup,
+                 .message = "=== GEOGRAPHIC DATA OVERVIEW ==="});
 
-  const std::vector<City>& all_locations =
-      curated_data_service_->LoadLocations();
+   const std::vector<City>& all_locations =
+       curated_data_service_->LoadLocations();
 
-  const size_t sample_count = std::min(
-      static_cast<size_t>(application_options_.pipeline.location_count),
-      all_locations.size());
+   const size_t sample_count = std::min(
+       static_cast<size_t>(application_options_.pipeline.location_count),
+       all_locations.size());
 
-  const auto sample_count_signed =
-      static_cast<std::iter_difference_t<decltype(all_locations.cbegin())>>(
-          sample_count);
+   const auto sample_count_signed =
+       static_cast<std::iter_difference_t<decltype(all_locations.cbegin())>>(
+           sample_count);
 
-  std::vector<City> sampled_locations;
-  sampled_locations.reserve(sample_count);
+   std::vector<City> sampled_locations;
+   sampled_locations.reserve(sample_count);
 
-  std::random_device random_generator;
-  std::ranges::sample(all_locations, std::back_inserter(sampled_locations),
-                      sample_count_signed, random_generator);
+   std::random_device random_generator;
+   std::ranges::sample(all_locations, std::back_inserter(sampled_locations),
+                       sample_count_signed, random_generator);
 
-  logger_->Log({.level = LogLevel::Info,
-                .phase = PipelinePhase::Startup,
-                .message = std::format("  Locations available: {}",
-                                       all_locations.size())});
-  logger_->Log({.level = LogLevel::Info,
-                .phase = PipelinePhase::Startup,
-                .message = std::format("  Sampled locations: {}",
-                                       sampled_locations.size())});
-  return sampled_locations;
+   logger_->Log({.level = LogLevel::Info,
+                 .phase = PipelinePhase::Startup,
+                 .message = std::format("  Locations available: {}",
+                                        all_locations.size())});
+   logger_->Log({.level = LogLevel::Info,
+                 .phase = PipelinePhase::Startup,
+                 .message = std::format("  Sampled locations: {}",
+                                        sampled_locations.size())});
+   return sampled_locations;
 }
