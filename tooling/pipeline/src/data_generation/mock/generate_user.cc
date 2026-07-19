3d7df14d5cbd4d7f2e9aa87b5424cb5302ebac38
Aaron Po <apo2@uwo.ca>
2026-07-19 04:10:33 -0400

    Formatted src/data_generation/mock

--- tooling/pipeline/src/data_generation/mock/generate_user.cc
+++ tooling/pipeline/src/data_generation/mock/generate_user.cc
@@ -11,21 +11,21 @@
 UserResult MockGenerator::GenerateUser(const EnrichedCity& city,
                                        const UserPersona& persona,
                                        const Name& name) {
-  const size_t hash = DeterministicHash(city.location, persona, name);
+   const size_t hash = DeterministicHash(city.location, persona, name);
 
-  const std::string_view username = kUsernames[hash % kUsernames.size()];
-  const std::string_view bio = kBios[hash / kBioHashStride % kBios.size()];
-  const float activity_weight =
-      static_cast<float>(hash / kActivityWeightHashStride %
-                         kActivityWeightHashRange) /
-      static_cast<float>(kActivityWeightHashRange);
+   const std::string_view username = kUsernames[hash % kUsernames.size()];
+   const std::string_view bio = kBios[hash / kBioHashStride % kBios.size()];
+   const float activity_weight =
+       static_cast<float>(hash / kActivityWeightHashStride %
+                          kActivityWeightHashRange) /
+       static_cast<float>(kActivityWeightHashRange);
 
-  return {
-      .first_name = name.first_name,
-      .last_name = name.last_name,
-      .gender = name.gender,
-      .username = std::string(username),
-      .bio = std::string(bio),
-      .activity_weight = activity_weight,
-  };
+   return {
+       .first_name = name.first_name,
+       .last_name = name.last_name,
+       .gender = name.gender,
+       .username = std::string(username),
+       .bio = std::string(bio),
+       .activity_weight = activity_weight,
+   };
 }
