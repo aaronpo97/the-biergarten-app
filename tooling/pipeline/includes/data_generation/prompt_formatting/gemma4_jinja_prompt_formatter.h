507761bc9a1ed855cfd3dcebcc4e4b947ea62f8f
Aaron Po <apo2@uwo.ca>
2026-07-19 04:10:32 -0400

    Formatted includes/data_generation/prompt_formatting

--- tooling/pipeline/includes/data_generation/prompt_formatting/gemma4_jinja_prompt_formatter.h
+++ tooling/pipeline/includes/data_generation/prompt_formatting/gemma4_jinja_prompt_formatter.h
@@ -7,12 +7,13 @@
 #include "data_generation/prompt_formatting/prompt_formatter.h"
 
 class Gemma4JinjaPromptFormatter final : public IPromptFormatter {
- public:
-  Gemma4JinjaPromptFormatter() = default;
-  ~Gemma4JinjaPromptFormatter() override = default;
+  public:
+   Gemma4JinjaPromptFormatter() = default;
+   ~Gemma4JinjaPromptFormatter() override = default;
 
-  [[nodiscard]] std::string Format(std::string_view system_prompt,
-                                   std::string_view user_prompt) const override;
+   [[nodiscard]] std::string Format(
+       std::string_view system_prompt,
+       std::string_view user_prompt) const override;
 };
 
 #endif  // BIERGARTEN_PIPELINE_INCLUDES_DATA_GENERATION_PROMPT_FORMATTING_GEMMA4_JINJA_PROMPT_FORMATTER_H_
