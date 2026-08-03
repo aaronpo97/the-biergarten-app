cc7a6486d1aea0dd2a4e0625c043203462f60dcb
Aaron Po <apo2@uwo.ca>
2026-08-03 02:47:06 -0400

    Update database migration and seeding logic; adjust Docker port mapping

--- web/backend/Database/Database.Seed/Program.cs
+++ web/backend/Database/Database.Seed/Program.cs
@@ -1,3 +1,5 @@
+
+
 using Database.Seed.DatabaseHelpers;
 using Database.Seed.PipelineData;
 using Database.Seed.Sqlite;
@@ -10,74 +12,67 @@ using Microsoft.Data.Sqlite;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Database.Seed;
 
-public class Program
+IReadOnlyList<BreweryRecord> breweries;
+IReadOnlyList<UserRecord> users;
+try
 {
-    public static int Main()
-    {
-        IReadOnlyList<BreweryRecord> breweries;
-        IReadOnlyList<UserRecord> users;
-        try
-        {
-            PipelineSeedDataReader reader = new(ConnectionStrings.SqliteConnectionString);
-            breweries = reader.ReadBreweryRecords();
-            users = reader.ReadUserRecords();
-        }
-        catch (SqliteException ex)
-        {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine($"Error opening database connection: {ex.Message}");
-            Console.ResetColor();
-            return 1;
-        }
-
-        ServiceCollection services = new();
-        services.AddSingleton<IConfiguration>(new ConfigurationBuilder().Build());
-        services.AddSingleton<ISqlConnectionFactory, DefaultSqlConnectionFactory>();
-        services.AddFeaturesBreweries();
-        services.AddFeaturesAuth();
-
-        using ServiceProvider provider = services.BuildServiceProvider();
-
-        IBreweryRepository breweryRepository;
-        IAuthRepository authRepository;
-        try
-        {
-            breweryRepository = provider.GetRequiredService<IBreweryRepository>();
-            authRepository = provider.GetRequiredService<IAuthRepository>();
-        }
-        catch (InvalidOperationException ex)
-        {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine($"Error configuring database connection: {ex.Message}");
-            Console.ResetColor();
-            return 1;
-        }
-
-        Console.ForegroundColor = ConsoleColor.Green;
-        Console.WriteLine(
-            $"Loaded {breweries.Count} breweries."
-        );
-        Console.ResetColor();
-
-        for (var i = 0; i < breweries.Count; i++)
-        {
-            BreweryRecord brewery = breweries[i];
-            Console.WriteLine(
-                $"{i + 1}:\t{brewery.Brewery.NameEn}\t({brewery.Address.City.CityName}, {brewery.Address.City.Country})");
-        }
-
-        Console.ForegroundColor = ConsoleColor.Green;
-        Console.WriteLine($"Loaded {users.Count} users.");
-        Console.ResetColor();
-
-        for (var i = 0; i < users.Count; i++)
-        {
-            UserRecord user = users[i];
-            Console.WriteLine($"{i + 1}:\t{user.User.FirstName} {user.User.LastName}\t ({user.Email})");
-        }
-
-        return 0;
-    }
+    PipelineSeedDataReader reader = new(ConnectionStrings.SqliteConnectionString);
+    breweries = reader.ReadBreweryRecords();
+    users = reader.ReadUserRecords();
 }
+catch (SqliteException ex)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine($"Error opening database connection: {ex.Message}");
+    Console.ResetColor();
+    return 1;
+}
+
+ServiceCollection services = [];
+services.AddSingleton<IConfiguration>(new ConfigurationBuilder().Build());
+services.AddSingleton<ISqlConnectionFactory, DefaultSqlConnectionFactory>();
+services.AddFeaturesBreweries();
+services.AddFeaturesAuth();
+
+using ServiceProvider provider = services.BuildServiceProvider();
+
+IBreweryRepository breweryRepository;
+IAuthRepository authRepository;
+try
+{
+    breweryRepository = provider.GetRequiredService<IBreweryRepository>();
+    authRepository = provider.GetRequiredService<IAuthRepository>();
+}
+catch (InvalidOperationException ex)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine($"Error configuring database connection: {ex.Message}");
+    Console.ResetColor();
+    return 1;
+}
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine(
+    $"Loaded {breweries.Count} breweries."
+);
+Console.ResetColor();
+
+for (int i = 0; i < breweries.Count; i++)
+{
+    BreweryRecord brewery = breweries[i];
+    Console.WriteLine(
+        $"{i + 1}:\t{brewery.Brewery.NameEn}\t({brewery.Address.City.CityName}, {brewery.Address.City.Country})");
+}
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine($"Loaded {users.Count} users.");
+Console.ResetColor();
+
+for (int i = 0; i < users.Count; i++)
+{
+    UserRecord user = users[i];
+    Console.WriteLine($"{i + 1}:\t{user.User.FirstName} {user.User.LastName}\t ({user.Email})");
+}
+
+return 0;
